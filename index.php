81c76efa2a60bf7dc338bfc218749871f59e71cd
Andy Sweetman <adsweetman@gmail.com>
2025-10-12 11:29:48 +0100

    AS - Learning

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 
-require_once __DIR__.'/my_functions.php';
+$colours = ['Red', 'Green', 'Blue', 'Yellow', 'Orange'];
 
+$ages = [23,31,55];
+
+$sum = array_sum($ages);
+$count = count($ages);
+
+print "Sum : {$sum}\n";
+print "Count : {$count}\n";
+
+print (array_sum($ages) / count($ages)) . "\n";
+
+print $colours[array_rand($colours)] . "\n";
+
+foreach($colours as $colour_key => $colour_value){
+    $colour_value_lower = strtolower($colour_value);
+    print "colour {$colour_key} - {$colour_value_lower} \n";
+}
